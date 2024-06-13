72d445052f3a3c55cb27ed0f9201a2f6bc221128
rmacohen <127235776+rmacohen@users.noreply.github.com>
2024-06-13 02:27:27 +0000

    Deploying to gh-pages from @ remarkablegames/psych-101@82a9fb557dac2a28df3760f8d2cdca911e99380e 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'renpy-template';
+var cacheName = 'psych-101';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
